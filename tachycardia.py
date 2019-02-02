4d44b16452b0d17d88168ee89eba7bc280a3a439
Nate Watson <nate.watson@duke.edu>
2019-02-02 13:39:10 -0500

    Added ability to remove leading/trailing white space

---
+++ tachycardia.py
@@ -0,0 +1,12 @@
+
+def is_tachycardia(string_input):
+    compare = "tachycardic"  # Word to compare strings
+
+    string_input = str(string_input)  # Ensure input is a valid string
+    updated_input = string_input.casefold()  # Convert to lowercase
+    updated_input = updated_input.strip()  # Remove trailing/leading spaces
+
+    if (updated_input == compare):  # Determine if input matches
+        return True
+    else:
+        return False
