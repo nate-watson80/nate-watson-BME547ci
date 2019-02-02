49aa69119dc660a1d88b9fc5ee682c9795e049ce
Nate Watson <nate.watson@duke.edu>
2019-02-02 14:00:00 -0500

    Added additional testing for bonus functionality

--- test_tachycardia.py
+++ test_tachycardia.py
@@ -1,4 +1,5 @@
 import pytest
+from tachycardia import is_tachycardia
 
 
 @pytest.mark.parametrize("string_input,expected", [  # Basic functionality
@@ -7,9 +8,24 @@ import pytest
     (" tachycardic", True),
     ("tachycardic ", True),
     ("tachYcaRdic", True),
+    ("          tachycardic", True),
+    (1234, False),
+    ("Wrong-word", False),
+    ("%$#", False),
 ])
 def test_is_tachycardia(string_input, expected):
-    from tachycardia import is_tachycardia
+    result = is_tachycardia(string_input)
+
+    assert result == expected
+
+
+@pytest.mark.parametrize("string_input,expected", [  # Enhanced functionality
+    ("tachyardic", True),
+    ("tachycard1c", True),
+    ("achycardIc", True),
+    ("  TacHcard1c", True),
+])
+def test2_is_tachycardia(string_input, expected):
     result = is_tachycardia(string_input)
 
     assert result == expected
