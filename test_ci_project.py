82f0ebd3e1165fb2847a7e3900ec8258f8a7f4fc
Nate Watson <nate.watson@duke.edu>
2019-02-02 12:50:34 -0500

    Added basic functionality on testing module

--- test_ci_project.py
+++ test_ci_project.py
@@ -1,2 +1,16 @@
-def test_is_tachycardia(string_input):
-    
+import pytest
+
+@pytest.mark.parametrize("string_input,expected",[
+    ("tachycardia", True),
+    ("Tachycardia", True),
+    (" Tachycardia", True),
+    ("tachycardia ", True),
+    ("tachYcaRdia", True),
+
+])
+def test_is_tachycardia(string_input, expected):
+from tachycardia import is_tachycardia
+
+result = is_tachycardia(string_input)
+
+assert result == expected
